e6148d4508b4dad0147e7d8cf33688961bcab530
David Chen <davidchen@gliacloud.com>
2025-02-03 13:20:53 +0800

    fix #185 ci (#186)
    * fix #185 ci
    
    * fix
    
    * fix

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# basic reference for writing script for travis
-
-set -ev
-
-poetry export --without-hashes -f requirements.txt -o requirements.txt
-poetry export --without-hashes --with dev -f requirements.txt -o requirements-test.txt
